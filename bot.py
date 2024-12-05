31975a4dacd5934a60ef4c4113c8f4034186d27e
Ravenium22 <128934893+Ravenium22@users.noreply.github.com>
2024-12-05 19:04:18 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -4,14 +4,19 @@ from discord.ext import commands
 
 intents = discord.Intents.default()
 intents.message_content = True
+
 bot = commands.Bot(command_prefix='!', intents=intents)
 
 @bot.event
 async def on_ready():
-    print(f'Bot is running as {bot.user}')
+    print(f'Bot is collecting messages')
+    await collect_messages()
 
-if __name__ == "__main__":
-    TOKEN = os.environ.get('DISCORD_TOKEN')
-    if not TOKEN:
-        raise ValueError("No token found. Set DISCORD_TOKEN environment variable")
-    bot.run(TOKEN)
+async def collect_messages():
+    CHANNELS = ['91252878206495883274', '1163480509146464298']
+    for channel_id in CHANNELS:
+        channel = bot.get_channel(int(channel_id))
+        async for msg in channel.history(limit=None):
+            print(f"Message collected: {msg.content[:50]}...")
+
+bot.run(os.environ['DISCORD_TOKEN'])
