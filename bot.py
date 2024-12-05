0293278d81c1e3fa82bf0a65b4e34b833dcce2a6
Ravenium22 <128934893+Ravenium22@users.noreply.github.com>
2024-12-05 18:52:05 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -4,12 +4,14 @@ from discord.ext import commands
 
 intents = discord.Intents.default()
 intents.message_content = True
-
 bot = commands.Bot(command_prefix='!', intents=intents)
 
 @bot.event
 async def on_ready():
     print(f'Bot is running as {bot.user}')
 
-TOKEN = os.getenv('DISCORD_TOKEN')
-bot.run(TOKEN)
+if __name__ == "__main__":
+    TOKEN = os.environ.get('DISCORD_TOKEN')
+    if not TOKEN:
+        raise ValueError("No token found. Set DISCORD_TOKEN environment variable")
+    bot.run(TOKEN)
