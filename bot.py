4b04bde60c11f19bbdc69e0a2aea039ea0a5d6bc
Ravenium22 <128934893+Ravenium22@users.noreply.github.com>
2024-12-05 18:43:57 +0300

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,15 @@
+import os
+import discord
+from discord.ext import commands
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(command_prefix='!', intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f'Bot is running as {bot.user}')
+
+TOKEN = os.getenv('DISCORD_TOKEN')
+bot.run(TOKEN)
